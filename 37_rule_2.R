bb494f8627c1dab6570d6dda60cc9d92398b509c
Uthistra K <100072615+Uthistra@users.noreply.github.com>
2022-11-13 01:35:23 +0100

    Add files via upload
    Optimal number value calc  is added

---
+++ 37_rule_2.R
@@ -0,0 +1,43 @@
+library(ggplot2)
+#37%rule
+sims <- 10000
+p <- c()
+p[1] <- 0
+
+for (k in 2:999) {
+  success <- 0
+  for (n in 1:sims) {
+    fun1 <- function(x) 1/x
+    x <- sample(1:1000, 1000)
+    init_best <- max(x[1:k])
+    candidate_score = -1
+    for (i in (k+1):1000) {
+      if (x[i] > init_best) {
+        candidate_score <- x[i]
+        break
+      }
+    }
+    if (candidate_score == 1000)
+      success <- success + 1
+  }
+  p[k] <- success/sims
+  cat(k, p[k] , " complete \n")
+  
+  
+  
+}
+plot(1:999, p, type = "l", xlab = "Omitted Candidates",
+     ylab = "Probability of Best Candidate Selection",
+     main = "The Secretary Problem")
+
+
+################ optimistic solution
+
+library(ggplot2)
+Fr <- function(r,N=10000){
+  
+  (r/N)*sum(1/(seq(from=r,to=N-1)))
+  
+  
+}
+optimize(Fr,interval = c(1:10000),maximum=TRUE)
